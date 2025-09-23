91cb69c817231cb88a002539b905accf88400145
SewarAslan <sewaraslan02@gmail.com>
2025-09-23 15:32:20 +0300

    adding the button component

---
+++ my-app/src/components/Button/Button.js
@@ -0,0 +1,44 @@
+import "./Button.css";
+
+export default function Button({ color, text, size }) {
+  const sizeStyle = {
+    small: {
+      padding: "4px 10px",
+      fontSize: "12px",
+    },
+    medium: {
+      padding: "6px 16px",
+      fontSize: "15px",
+    },
+    large: {
+      padding: "10px 100px",
+      fontSize: "16px",
+    },
+  };
+  const colorStyle = {
+    blue: {
+      backgroundColor: "#1d9bf0ff",
+      color: "white",
+    },
+    black: {
+      backgroundColor: "black",
+      color: "white",
+    },
+    white: {
+      backgroundColor: "white",
+      color: "black",
+      border: "1px solid #ccc",
+    },
+  };
+
+  return (
+    <button
+      style={{
+        ...sizeStyle[size],
+        ...colorStyle[color],
+      }}
+    >
+      {text}
+    </button>
+  );
+}
